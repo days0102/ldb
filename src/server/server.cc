3fae8e96b01be8407e28f793601fd29aa07bfa9b
days0102 <411883104@qq.com>
2023-02-24 16:16:43 +0800

    CMakeLists

--- src/server/server.cc
+++ src/server/server.cc
@@ -2,10 +2,12 @@
  * @Author: Outsider
  * @Date: 2023-02-24 11:15:56
  * @LastEditors: Outsider
- * @LastEditTime: 2023-02-24 15:31:30
+ * @LastEditTime: 2023-02-24 16:03:22
  * @Description: In User Settings Edit
  * @FilePath: /ldb/src/server/server.cc
  */
+#include <iostream>
 auto main() -> int
 {
+    std::cout << "server" << std::endl;
 }
