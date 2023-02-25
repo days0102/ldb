840b823fffb93fe4812f696aea2a9b487f007390
days0102 <411883104@qq.com>
2023-02-25 09:54:51 +0800

    code format

--- src/client/client.cc
+++ src/client/client.cc
@@ -1,18 +1,37 @@
 /*
- * @Author: Outsider
- * @Date: 2023-02-24 11:06:31
- * @LastEditors: Outsider
- * @LastEditTime: 2023-02-24 13:19:56
- * @Description: In User Settings Edit
- * @FilePath: /ldb/client.cc
+ * @Author       : Outsider
+ * @Date         : 2023-02-24 11:06:31
+ * @LastEditors  : Outsider
+ * @LastEditTime : 2023-02-25 09:52:35
+ * @Description  : In User Settings Edit
+ * @FilePath     : /ldb/src/client/client.cc
  */
-#include <iostream>
 #include <getopt.h>
-auto main(int argc, char *argv[]) -> int
-{
-    int opt;
-    while ((opt = getopt(argc, argv, "p:")) > 0)
-    {
-        std::cout << opt << std::endl;
+#include <netdb.h>
+#include <stdlib.h>
+
+#include <iostream>
+
+/**
+ * @description: tcp socket connect to host:port
+ * @param  *host
+ * @param  port
+ * @return socket fd if success
+ */
+int tcpsock(const char *host, int port) { struct hostent server_host; }
+
+auto main(int argc, char *argv[]) -> int {
+  int defport = 9901;
+  int opt;
+  extern char *optarg;
+  while ((opt = getopt(argc, argv, "p:")) > 0) {
+    switch (opt) {
+      case 'p':
+        defport = atoi(optarg);
+        break;
+
+      default:
+        break;
     }
+  }
 }
--- src/server/server.cc
+++ src/server/server.cc
@@ -1,13 +1,11 @@
 /*
- * @Author: Outsider
- * @Date: 2023-02-24 11:15:56
- * @LastEditors: Outsider
- * @LastEditTime: 2023-02-24 16:03:22
- * @Description: In User Settings Edit
- * @FilePath: /ldb/src/server/server.cc
+ * @Author       : Outsider
+ * @Date         : 2023-02-24 11:15:56
+ * @LastEditors  : Outsider
+ * @LastEditTime : 2023-02-25 09:53:29
+ * @Description  : In User Settings Edit
+ * @FilePath     : /ldb/src/server/server.cc
  */
+
 #include <iostream>
-auto main() -> int
-{
-    std::cout << "server" << std::endl;
-}
+auto main() -> int { std::cout << "server" << std::endl; }
